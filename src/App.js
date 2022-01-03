652d6b5904159179023121c8d66f21d55a0e84e0
moyamoha <82891624+moyamoha@users.noreply.github.com>
2022-01-03 06:55:04 +0200

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home'
 import BreweryDetail from './components/BreweryDetail'
 import './App.css';
